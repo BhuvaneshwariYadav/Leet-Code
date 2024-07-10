af615e0aedf0ad653fc5de5a3405ae699035f552
P Bhuvaneshwari Yadav <bhuvaneshwari5m7@gmail.com>
2024-07-10 21:55:48 +0530

    Time: 1 ms (99.42%) | Memory: 42.6 MB (5.90%) - LeetSync

--- 1720-crawler-log-folder/crawler-log-folder.java
+++ 1720-crawler-log-folder/crawler-log-folder.java
@@ -1,21 +1,12 @@
 class Solution {
     public int minOperations(String[] logs) {
-        Stack<String> st=new Stack<>();
         int ans=0;
-        int k=1;
         for(String i:logs)
         {
             if(i.charAt(0)!='.')
-            {
-                st.push(k+"");
-                k++;
                 ans++;
-            }
             if(i.charAt(1)=='.' && ans!=0)
-            {
-                st.pop();
                 ans--;
-            }
         }
         return ans;
     }
